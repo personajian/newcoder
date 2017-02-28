6aeb9603443577d13b563a89b8f13c4ad1021526
personajian <personajian@gmail.com>
2017-02-28 20:54:39 +0800

    华为在线编程108题。

---
+++ src/cn/edu/seu/newcoder/onlinejudge/huawei/CountCharInString.java
@@ -0,0 +1,28 @@
+package cn.edu.seu.newcoder.onlinejudge.huawei;
+
+import java.util.Scanner;
+
+/**
+ * @author personajian
+ *
+ *写出一个程序，接受一个有字母和数字以及空格组成的字符串，和一个字符，然后输出输入字符串中含有该字符的个数。不区分大小写。
+ */
+public class CountCharInString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in= new Scanner(System.in);
+		while(in.hasNext()){
+			String str= in.nextLine().toLowerCase();
+			String character=in.nextLine().toLowerCase();
+			int count=0;
+			int fromIndex=0;
+			while(str.indexOf(character, fromIndex)!=-1){
+				count++;
+				fromIndex=str.indexOf(character, fromIndex)+1;
+			}
+			System.out.println(count);
+		}
+	}
+
+}
---
+++ src/cn/edu/seu/newcoder/practice/java/Choose20170228.java
@@ -0,0 +1,10 @@
+package cn.edu.seu.newcoder.practice.java;
+
+public class Choose20170228 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.round(11.5));
+	}
+
+}
